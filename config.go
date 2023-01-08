4f30b0e47a0cfa348e5c54a20fbbc1183554ba8e
mihonen <villemihonen@yahoo.fi>
2023-01-08 02:06:00 +0200

    change default rounding decimal places to 12

--- config.go
+++ config.go
@@ -11,7 +11,7 @@ package polynomials
 const (
 	durandKernerMaxIter = 5000
 	maxNewtonIterations = 25
-	roundingDecimalPlaces = 9
+	roundingDecimalPlaces = 12
 	epsNewton = 1e-10 // max error allowed
 	epsDurand = 1e-16
 	defaultSolvingMethod = Eigenvalue
